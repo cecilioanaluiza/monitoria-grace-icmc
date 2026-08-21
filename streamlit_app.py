2fc55689c6537a8f25b7cf1c07f2e14142bdfb2d
cecilioanaluiza <cecilioanaluiza@gmail.com>
2026-08-21 00:02:30 +0000

    Primeiro commit do projeto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Controle de tarefas!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
